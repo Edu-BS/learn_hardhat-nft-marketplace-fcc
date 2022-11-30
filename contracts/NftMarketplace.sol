d7b97ac3617d68ad81c630224f3d4abadd76f8fb
Edu-BS <edu.bs.email@gmail.com>
2022-11-30 16:20:27 +0100

    Add getters in NftMarketplace

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -160,4 +160,20 @@ contract NftMarketplace is ReentrancyGuard {
             revert NftMarketplace__TransferFailed();
         }
     }
+
+    /**
+     * Getters
+     */
+
+    function getListing(address nftAddress, uint256 tokenId)
+        external
+        view
+        returns (Listing memory)
+    {
+        return s_listings[nftAddress][tokenId];
+    }
+
+    function getProceeds(address seller) external view returns (uint256) {
+        return s_proceeds[seller];
+    }
 }
