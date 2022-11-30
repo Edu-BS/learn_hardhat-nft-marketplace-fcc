829a6ac6a7b71ee052c063da6db9ade1e261a323
Edu-BS <edu.bs.email@gmail.com>
2022-11-30 14:13:06 +0100

    Start NftMarketplace contract and add listItem method

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketplace_PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
+
+contract NftMarketplace {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    /**
+     * Modifiers
+     */
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    /**
+     *  Main functions
+     */
+
+    /**
+     * @notice Method for listing your NFT on the marketplace
+     * @param nftAddress: Address of the NFT
+     * @param tokenId: The Token ID of the NFT
+     * @param price: sale price of the listed NFT
+     * @dev Tecnically, we could have the contract be the escrow for the NFTs
+     * but this way people can still hold their NFTs when listedj
+     */
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        notListed(nftAddress, tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) {
+            revert NftMarketplace_PriceMustBeAboveZero();
+        }
+
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+}
