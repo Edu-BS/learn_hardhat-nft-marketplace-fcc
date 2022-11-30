d9ae953e185001a0a09ed8d0837da5028bd8d690
Edu-BS <edu.bs.email@gmail.com>
2022-11-30 16:08:36 +0100

    Add cancelListing() in NftMarketplace

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -32,6 +32,8 @@ contract NftMarketplace is ReentrancyGuard {
         uint256 price
     );
 
+    event ItemCanceled(address indexed seller, address indexed nftAddress, uint256 tokenId);
+
     // NFT Contract address -> NFT TokenId -> Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
 
@@ -125,5 +127,14 @@ contract NftMarketplace is ReentrancyGuard {
         IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
 
         emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+
+    function cancelListing(address nftAddress, uint256 tokenId)
+        external
+        isOwner(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId)
+    {
+        delete (s_listings[nftAddress][tokenId]);
+        emit ItemCanceled(msg.sender, nftAddress, tokenId);
     }
 }
